d115bc794107f0e8a2495cdef832c746dd9a1ec4
Dylan Deyal <dylandd13@outlook.com>
2020-07-27 19:13:22 -0400

    Filled in migrations

--- protected/migrations/m200727_201550_event_type_sort.php
+++ protected/migrations/m200727_201550_event_type_sort.php
@@ -1,15 +1,27 @@
 <?php
 
-class m200727_201550_event_type_sort extends CDbMigration
+class m200727_201550_event_type_sort extends OEMigration
 {
 	public function up()
 	{
+		$this->addColumn('event_type', 'sort_order', 'int');
+		$this->update('event_type', array('sort_order' => 99));
+		$this->update('event_type', array('sort_order' => 1), "id=27");
+		$this->update('event_type', array('sort_order' => 2), "id=40");
+		$this->update('event_type', array('sort_order' => 3), "id=32");
+		$this->update('event_type', array('sort_order' => 4), "id=4");
+		$this->update('event_type', array('sort_order' => 5), "id=14");
+		$this->update('event_type', array('sort_order' => 6), "id=37");
+		$this->update('event_type', array('sort_order' => 6), "id=36");
+
+
 	}
 
 	public function down()
 	{
 		echo "m200727_201550_event_type_sort does not support migration down.\n";
-		return false;
+		$this->dropColumn('event_type', 'sort_order');
+		
 	}
 
 	/*
