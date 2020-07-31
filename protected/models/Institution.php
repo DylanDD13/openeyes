554187a5e87b847e37eb9ce0cab00bddaea43002
Dylan Deyal <dylandd13@outlook.com>
2020-07-30 23:20:36 -0400

    Changed institute code to NUCL

--- protected/models/Institution.php
+++ protected/models/Institution.php
@@ -136,7 +136,7 @@ class Institution extends BaseActiveRecordVersioned
             throw new Exception('Institution code is not set');
         }
 
-        $institution = $this->find('source_id=? and remote_id=?', array(1, 181));
+        $institution = $this->find('source_id=? and remote_id=?', array(1, Yii::app()->params['institution_code']));
         if (!$institution) {
             throw new Exception("Institution with code '".Yii::app()->params['institution_code']."' not found");
         }
