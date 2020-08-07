6eeb3b65fe01f92ec9decf5ffa0d510e875af46e
DylanDD13 <DylanDD13@outlook.com>
2020-08-06 23:10:05 -0400

    Create NU Migration file

---
+++ protected/migrations/m200807_030925_nu_eyev1.php
@@ -0,0 +1,25 @@
+<?php
+
+class m200807_030925_nu_eyev1 extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m200807_030925_nu_eyev1 does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
