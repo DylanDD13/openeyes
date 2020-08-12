2a87ba56cc3745205ed8d45dea6ed6408c5880b3
Dylan Deyal <dylandd13@outlook.com>
2020-08-12 00:04:20 -0400

    Modified migration

--- protected/migrations/m200811_181232_nu_examination.php
+++ protected/migrations/m200811_181232_nu_examination.php
@@ -4,6 +4,15 @@ class m200811_181232_nu_examination extends CDbMigration
 {
 	public function up()
 	{
+		$this->insert('ophciexamination_element_set_item', array(
+			'set_id' => 1,
+			'element_type_id' => 449			
+		));
+		$this->insert('ophciexamination_element_set_item', array(
+			'set_id' => 1,
+			'element_type_id' => 355			
+		));
+		$this->delete('ophciexamination_element_set_item', 'element_type_id = 312');
 	}
 
 	public function down()
