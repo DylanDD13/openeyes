f729266094047dd72f8284bbbc80685499b850f6
DylanDD13 <DylanDD13@outlook.com>
2020-08-26 01:10:54 -0400

    Added migration for default eye color in eyedraw enterior segment

---
+++ protected/migrations/m200826_051017_eyedraw_default_color.php
@@ -0,0 +1,25 @@
+<?php
+
+class m200826_051017_eyedraw_default_color extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m200826_051017_eyedraw_default_color does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
