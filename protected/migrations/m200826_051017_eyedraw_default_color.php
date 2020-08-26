17c2245b86ac1e2a03ddf1a6048eadf51568bc19
Dylan Deyal <dylandd13@outlook.com>
2020-08-26 01:14:06 -0400

    Filled out eyedraw default colour migration

--- protected/migrations/m200826_051017_eyedraw_default_color.php
+++ protected/migrations/m200826_051017_eyedraw_default_color.php
@@ -4,6 +4,7 @@ class m200826_051017_eyedraw_default_color extends CDbMigration
 {
 	public function up()
 	{
+		$this->update('eyedraw_doodle', array('init_doodle_json' => '{"scaleLevel": 1,"version":1.1,"subclass":"AntSeg","pupilSize":"Large","apexY":-260,"rotation":0,"pxe":false,"colour":"Brown","coloboma":false,"ectropion":false,"cornealSize":"Not Checked","cells":"Not Checked","flare":"Not Checked","order":1}'), "eyedraw_class_mnemonic='AntSeg'");
 	}
 
 	public function down()
