58245e6ee4c7a1a79b9fd07bc9f66ce608ca79f3
DylanDD13 <DylanDD13@outlook.com>
2020-08-31 23:09:39 -0400

    IOL Migration Added

---
+++ protected/migrations/m200901_030913_iol_type.php
@@ -0,0 +1,25 @@
+<?php
+
+class m200901_030913_iol_type extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m200901_030913_iol_type does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
