8bd43d54df8c157b10c1d6bd7cf9c3cfc9bec8e3
DylanDD13 <DylanDD13@outlook.com>
2020-09-22 23:49:05 -0400

    Added commit for new disorders

---
+++ protected/migrations/m200923_034839_add_new_disorders.php
@@ -0,0 +1,25 @@
+<?php
+
+class m200923_034839_add_new_disorders extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m200923_034839_add_new_disorders does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
